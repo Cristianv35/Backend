97b7b10fcebe3546b7210c4d0d3821b204d71d17
Cristian Valenciano <cristianv35@hotmail.com>
2019-07-21 15:48:09 -0600

    Primera entrega

---
+++ js/index.js
@@ -0,0 +1,16 @@
+
+
+function inicializarSlider(){
+  $("#rangoPrecio").ionRangeSlider({
+    type: "double",
+    grid: false,
+    min: 0,
+    max: 100000,
+    from: 200,
+    to: 80000,
+    prefix: "$"
+  });
+}
+
+inicializarSlider();
+
